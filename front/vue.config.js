3ca1dbfaefb5ea527d1204f8b2833ada807ec2ff
kimtaeyoon <90013740+mangtaeeee@users.noreply.github.com>
2022-03-29 14:28:28 +0900

    Update vue.config.js

--- front/vue.config.js
+++ front/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             "/api":{
-                target:"http://localhost:19090", //spring boot 다른ip 를 못들오게 해서 이 proxy를 통해서 다른 로컬 값 들오게 만들어줌
+                target:"http://localhost:****", //spring boot 다른ip 를 못들오게 해서 이 proxy를 통해서 다른 로컬 값 들오게 만들어줌
                 pathRewrite: {
                     "^/api":"" 
                 }
